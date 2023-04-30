d0e61aa025d3a69584c0be59c4d075fe83e890dc
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-04-30 10:45:01 -0400

    Add cypress and home spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user 
+    else 
+      return nil
+    end 
+  end
+
+  validates :name, presence: true
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }
   
 end
